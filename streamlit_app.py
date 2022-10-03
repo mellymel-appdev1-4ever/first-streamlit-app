971796741abd8fde2409b5a26c054a4c45710d54
mellymel-appdev1-4ever <114918217+mellymel-appdev1-4ever@users.noreply.github.com>
2022-10-03 18:01:56 +0530

    Update streamlit_app.py
    Adding orders

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bulberry Oat meal')
+streamlit.text('kate, Spinach & Rocket Smothie')
+streamit.text('Hard-Boiled free-Range Egg')
